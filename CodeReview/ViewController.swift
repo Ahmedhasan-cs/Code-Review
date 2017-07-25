d95ea6213e4af1c8f458fae27ba03b71b5481029
Aly <aly@parent.eu>
2017-07-25 23:00:26 +0200

    First commit

---
+++ CodeReview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeReview
+//
+//  Created by Ahmed Aly on 7/25/17.
+//  Copyright © 2017 Parent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
